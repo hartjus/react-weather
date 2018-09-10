ca4615dd7b3e134a904b88ac25d9517c9c472a54
Justin Hart <justingh@gmail.com>
2018-09-09 20:33:18 -0700

    Remove app key

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export default class WeatherService {
 
     async getForecast(zipCode) {
-        const url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&APPID=1b8dbfc46dac2cee980b295dc56c1534`;
+        const url = `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&APPID={APP_KEY}`;
 
         // returns a promise
         const result = await axios.get(url);
-        
+
         return result.data;
     }
 }
